296a11fcec732582a66ba868057ebc1047c936e7
AxisAlmond <almoneda.alecxis@gmail.com>
2025-11-13 20:26:01 +0000

    Defined PricingRespository class in order_respository.dart

--- lib/repositories/order_repository.dart
+++ lib/repositories/order_repository.dart
@@ -21,3 +21,22 @@ class OrderRepository {
     }
   }
 }
+
+
+class PricingRepository {
+  int _price = 0;
+  int _quantity = 0;
+  bool type = false;
+
+  int get quantity => _quantity;
+
+  int calculate(quantity, type) {
+    if (type) {
+      int _price = 5;
+    }
+    else{
+      int _price = 8;
+    }
+    return(_price * _quantity);
+  }
+}
