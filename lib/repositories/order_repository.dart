44aeb54aa7f86e91a46a7ddfa00d3b81fb9b3400
AxisAlmond <almoneda.alecxis@gmail.com>
2025-11-13 20:36:44 +0000

    Add PricingRepository tests to order_repository_test.dart

--- lib/repositories/order_repository.dart
+++ lib/repositories/order_repository.dart
@@ -24,19 +24,13 @@ class OrderRepository {
 
 
 class PricingRepository {
-  int _price = 0;
-  int _quantity = 0;
-  bool type = false;
-
-  int get quantity => _quantity;
-
-  int calculate(quantity, type) {
-    if (type) {
-      int _price = 5;
-    }
-    else{
-      int _price = 8;
-    }
-    return(_price * _quantity);
+  /// Simple pricing helper.
+  ///
+  /// Calculates total price for `quantity` sandwiches.
+  /// If [isFootlong] is true, the unit price is 11, otherwise 7.
+  int calculate(int quantity, bool isFootlong) {
+    final int unitPrice = isFootlong ? 11 : 7;
+    if (quantity <= 0) return 0;
+    return unitPrice * quantity;
   }
 }
